540367b70c9aaec06170c3d1d23372f068cff7e3
micio <miciek@gmail.com>
2012-02-14 00:15:34 +0100

    E-mail preferences changed

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,7 +5,7 @@ class UserMailer < ActionMailer::Base
   def new_match_time_proposal(proposal, receiving_team)
     @user = receiving_team.owner
     @proposal = proposal
-    mail(:to => "miciek@gmail.com", :subject => "Nowa propozycja terminu meczu " + @user.email)
+    mail(:to => @user.email, :bcc => "hq@unchartedleague.com", :subject => "Nowa propozycja terminu meczu")
   end
   
   def match_time_accepted(match)    
@@ -13,6 +13,6 @@ class UserMailer < ActionMailer::Base
     users = @match.team1.members + @match.team2.members
     users << @match.judge
     recipients = users.collect(&:email)
-    mail(:to => recipients, :subject => "Termin meczu #{@match.team1.full_tag}-#{@match.team2.full_tag} został ustalony!")
+    mail(:to => "hq@unchartedleague.com", :bcc => recipients, :subject => "Termin meczu #{@match.team1.full_tag}-#{@match.team2.full_tag} został ustalony!")
   end
 end
