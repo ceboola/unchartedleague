e79b899870c0c1efb06b3d811732ada04c234d17
micio <miciek@gmail.com>
2012-03-05 19:49:28 +0100

    CanCan for Rounds controller

--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -1,4 +1,6 @@
 class RoundsController < ApplicationController  
+  load_and_authorize_resource
+
   def create    
     @round = Round.new(params[:round])
     if @round.save
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,7 +3,8 @@ class Ability
 
   def initialize(user)
     user ||= User.new # guest user (not logged in)
-    can :update, Match, :judge_id => user.id
-    can :manage, MatchEntry, :match => { :judge_id => user.id }
+    if user.id == 1
+      can :manage, Round     
+    end
   end  
 end
